d43f9861461303010756af5ab52668015b8ed81e
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-10-13 10:13:02 +0200

    Add toString method

--- src/Ellipse.js
+++ src/Ellipse.js
@@ -1,7 +1,20 @@
+/**
+ * Ellipse class.
+ *
+ * @constructs src/Ellipse
+ * @author Pauliina Raitaniemi <pr222ja@student.lnu.se>
+ * @version 1.0.0
+ */
+
+// ------------------------------------------------------------------------------
+//  Public interface
+// ------------------------------------------------------------------------------
 export class Ellipse {
   constructor (a, b, area, circumference) {
     this.a = a
     this.b = b
+    this._area = area
+    this._circumference = circumference
   }
 
   get a () {
@@ -13,18 +26,18 @@ export class Ellipse {
   }
 
   get area () {
-    return this.calcArea()
+    return this._calcArea()
   }
 
-  calcArea () {
+  calcArea () { // Calculate the area using a and b
     return Math.PI * this.a * this.b
   }
 
   get circumference () {
-    return this.calcCircumference()
+    return this._calcCircumference()
   }
 
-  calcCircumference () {
+  calcCircumference () { // Calculate the circumference using a and b
     return Math.PI * Math.sqrt(2 * this.a * this.a * 2 * this.b * this.b)
   }
 
@@ -34,6 +47,10 @@ export class Ellipse {
 
   set b (value) {
     this.b = value
+  }
+
+  toString () { // Method that returns the representated object as a string.
+    return `a: ${this.a}, b: ${this.b}, area: ${this.area.toFixed(2)}, circumference: ${this.circumference.toFixed(2)}`
   }
 
   // Lägg till toString-metod
---
+++ src/app.js
@@ -0,0 +1,10 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Pauliina Raitaniemi <pr222ja@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import * as Ellipse from './Ellipse.js'
+
+// TODO: Add code here.
