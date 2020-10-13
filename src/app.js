8608e875c655a442357e95921bc508fcc94678c0
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-10-13 11:38:15 +0200

    Add representations with new values

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@
  */
 
 import { Ellipse } from './Ellipse.js'
+// Create new object an set initial values
 const ellipse = new Ellipse(42.7, 13.8)
+console.log(ellipse.toString()) // Present representation as a string
 
-console.log(ellipse.toString())
+// Change the values of the object
+ellipse.a = 63
+ellipse.b = 18.4
+console.log(ellipse.toString())// Present 2nd representation as a string
+
+// Change the values of the object again
+ellipse.a = 78.9
+ellipse.b = 68.4
+console.log(ellipse.toString()) // Present 3rd representation as a string
